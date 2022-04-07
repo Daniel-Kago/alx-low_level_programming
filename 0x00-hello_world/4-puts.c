98e3ddc776dd6de3a4be2ea441a4c1953c4552fc
Daniel <alxsoftwareengineering@gmail.com>
2022-04-07 02:34:10 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
-#include <stdio.h>                                                                                                               
-/**                                                                                                                              
-*main - entry point                                                                                                              
-*                                                                                                                                
-*Return: 0 after printing function                                                                                               
-*/                                                                                                                               
-int main(void)                                                                                                                   
+#include <stdio.h>
+
+/**
+*main - Entry poin
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
 {
-	puts("\"Programming is like a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
