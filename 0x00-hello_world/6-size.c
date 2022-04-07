a6d50e7f410e858b8dbe5a4a7d26c4127dcb8122
Daniel <alxsoftwareengineering@gmail.com>
2022-04-07 06:36:52 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,16 @@
-#include <stdio.h>                                                                                                             
-                                                                                                                               
-/**                                                                                                                            
-*main - Entry point                                                                                                            
-*                                                                                                                              
-*Return: Always 0  (Success)                                                                                                   
-*/                                                                                                                             
-int main(void)                                                                                                                 
-{                                                                                                                              
-int a;                                                                                                                         
-long int b;                                                                                                                    
-long long int c;                                                                                                               
-char d;                                                                                                                        
-float f;                                                                                                                       
-                                                                                                                               
-printf("Size of the char: %lu byte(s)\n",sizeof(d));                                                            
-printf("Size of an int: %lu byte(s)\n",sizeof(a));                                                              
-printf("size of long int: %lu byte(s)\n",sizeof(b));                                                            
-printf("size of long long int: %lu byte(s)\n",sizeof(c));                                                        
+#include <stdio.h>
+/**
+ * 
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void) 
+{
+  printf("Size of a char: %d byte(s)\n", sizeof(char)); 
+  printf("Size of a char: %d byte(s)\n", sizeof(int));
+  printf("Size of a char: %d byte(s)\n", sizeof(long int));
+  printf("Size of a char: %d byte(s)\n", sizeof(long long int));
+  printf("Size of a char: %d byte(s)\n", sizeof(float));
+  return (0);
 }
