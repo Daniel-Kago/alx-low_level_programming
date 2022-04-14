b1975d087a0edf31a9cbce7c55806e71905b0877
Daniel <alxsoftwareengineering@gmail.com>
2022-04-14 06:33:36 -0700

    Task 0

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,38 @@
+100-times_table.c  103-fibonacci.c  11-print_to_98.c  3-islower.c             6-abs.c               9-times_table.c              
+101-natural.c      104-fibonacci.c  1-alphabet.c      4-isalpha.c             7-print_last_digit.c  main.h                       
+root@eff844835da2:~/alx-low_level_programming/0x02-functions_nested_loops# cat main.h                                            
+#ifndef MAIN_H_INCLUDED                                                                                                          
+                                                                                                                                 
+#define MAIN_H_INCLUDED                                                                                                          
+                                                                                                                                 
+                                                                                                                                 
+                                                                                                                                 
+int _putchar(char c);                                                                                                            
+                                                                                                                                 
+void print_alphabet(void);                                                                                                       
+                                                                                                                                 
+void print_alphabet_x10(void);                                                                                                   
+                                                                                                                                 
+int _islower(int c);                                                                                                             
+                                                                                                                                 
+int _isalpha(int c);                                                                                                             
+                                                                                                                                 
+int print_sign(int n);                                                                                                           
+                                                                                                                                 
+int _abs(int n);                                                                                                                 
+                                                                                                                                 
+int print_last_digit(int n);                                                                                                     
+                                                                                                                                 
+void jack_bauer(void);                                                                                                           
+                                                                                                                                 
+void times_table(void);                                                                                                          
+                                                                                                                                 
+int add(int a, int b);                                                                                                           
+                                                                                                                                 
+void print_to_98(int n);                                                                                                         
+                                                                                                                                 
+void print_times_table(int n);                                                                                                   
+                                                                                                                                 
+                                                                                                                                 
+                                                                                                                                 
+#endif
